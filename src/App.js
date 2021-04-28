c8d04cebdb6ac6e11b015dd395194769aed76243
Roberto-04-dev <true4321king@gmail.com>
2021-04-28 19:32:10 -0100

    Initial commit.
    Adding the sctuture of the react-app.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="Roberto"
+          className="Roberto Carlos"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
