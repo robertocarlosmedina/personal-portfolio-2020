c6e09e9cd0f335500513db3163c78c273fd5390b
Roberto-04-dev <true4321king@gmail.com>
2021-04-28 19:38:08 -0100

    Changin little stuffs

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="Roberto Carlos"
-          href="https://reactjs.org"
+          className="App-link"
+          href="https://www.github.com/RobertoCarlosMedina/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Roberto Carlos Medina
         </a>
       </header>
     </div>
